6a4573fd64992bee3660077d7f6cbe78f79a79c9
carlosibo <carlos.iborra@hotmail.com>
2016-11-24 17:57:35 +0100

    Código fuente

---
+++ Python.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env Python
+# -*- coding: utf-8 -*-
+
+texto = raw_input("Introduzca su texto: ")
+x=0
+cadena = ""
+dirFichero = raw_input("Introduzca la ubicacion de su fichero: ")
+longitudclave = raw_input("Introduzca la longitud de la clave: ")
+fichero = open(dirFichero,'w')
+
+while x<= len(texto):
+    cadena = cadena + texto[x]
+    x = x + longitudclave
+
+fichero.write(cadena)
